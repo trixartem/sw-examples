ecea9c383cd441e54024ec8af0ed68dfae29a61d
Artem Kuvaldin <akuv@yandex-team.ru>
2017-04-14 00:56:49 +0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', type: 'index' });
 });
 router.get('/offline', function(req, res, next) {
-  res.render('offline', { title: 'Express' });
+  res.render('offline', { title: 'Express', type: 'offline' });
 });
 
 module.exports = router;
